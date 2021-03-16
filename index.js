f3d2c9f3b2b39570140550b8a7f0ef62ada0f072
undrfined <undrfined@gmail.com>
2021-03-16 14:44:55 +0200

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const VERSION = "1.0";
-
+    console.log("oki")
     const addLogos = () => {
 
     }
--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 console.log("Loading...")
 window.onload = () => {
-    fetch('https://raw.githubusercontent.com/undrfined/crowdvns/master/index.js', {})
+    fetch('https://raw.githubusercontent.com/undrfined/crowdvns/master/index.js?flush_cache=True', {})
         .then(resp => resp.text())
         .then((t) => {
             eval(t)
