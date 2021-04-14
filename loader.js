137f34adb92a7b363b96e0a7d01ae63659cf428f
undrfined <undrfined@gmail.com>
2021-04-14 12:00:53 +0300

    fucking firefox ahhh

--- loader.js
+++ loader.js
@@ -1,9 +1,16 @@
 console.log("Loading...")
-window.onload = () => {
-    fetch('https://raw.githubusercontent.com/undrfined/crowdvns/master/index.js?flush_cache=True', {})
+const load = () => {
+    console.log("onload!")
+    const headers = new Headers();
+    headers.append('pragma', 'no-cache');
+    headers.append('cache-control', 'no-cache');
+    fetch('https://raw.githubusercontent.com/undrfined/crowdvns/master/index.js?flush_cache=True&' + +new Date(), {
+        cache: 'no-store'
+    })
         .then(resp => resp.text())
         .then((t) => {
             eval(t)
         })
         .catch(console.error)
 }
+document.readyState === "complete" ? load() : window.onload = load
